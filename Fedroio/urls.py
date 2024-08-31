02e47ca4b253fbfd34467d1c44ae9211b765da50
paoli7612 <paoli7612@gmail.com>
2024-08-31 16:38:13 +0200

    image copertina pawns

--- Fedroio/urls.py
+++ Fedroio/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from . import views
 urlpatterns = [
@@ -26,4 +28,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('pawns/', include('pawns.urls')),
     path('quiz/', include('quiz.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
