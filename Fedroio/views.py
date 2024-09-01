b33b9ab122f4b7c36627540ac0d954c4eeb3a864
paoli7612 <paoli7612@gmail.com>
2024-09-01 09:32:19 +0200

    copertina

--- Fedroio/views.py
+++ Fedroio/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from pawns.models import Pawn
 
 def index(request):
     return redirect(reverse('home'))
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'pawns': Pawn.objects.filter(parent=None)[:2]
+    })
 
 def account(request):
     return render(request, 'registration/account.html')
