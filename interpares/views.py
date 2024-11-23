e82df33901c80c209f1d9f993ce248d37c10ae64
paoli7612 <paoli7612@gmail.com>
2024-11-23 06:14:38 +0100

    fix security

--- interpares/views.py
+++ interpares/views.py
@@ -1,12 +1,15 @@
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.messages import success
+from django.contrib.auth.decorators import login_required
 from .models import Tema
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'interpares/index.html')
 
+@login_required
 def write(request):
     if request.method == 'POST':
         tema, _ = Tema.objects.get_or_create(user=request.user)
@@ -15,6 +18,7 @@ def write(request):
         return redirect(reverse('interpares'))
     return render(request, 'interpares/write.html')
 
+@login_required
 def evaluate(request):
     tema = Tema.objects.exclude(user=request.user).order_by('?').first()
     if not tema:
